65a928a4555deb93fd423b0508150eb7ea78e45e
naemoo <gusdn0657@gmail.com>
2022-07-09 16:30:09 +0900

    chore: Configuration 분리

---
+++ src/main/kotlin/com/ssu/commerce/delivery/configs/CoreConfiguration.kt
@@ -0,0 +1,8 @@
+package com.ssu.commerce.delivery.configs
+
+import com.ssu.commerce.core.configs.EnableSsuCommerceCore
+import org.springframework.context.annotation.Configuration
+
+@EnableSsuCommerceCore
+@Configuration
+class CoreConfiguration
---
+++ src/main/kotlin/com/ssu/commerce/delivery/configs/VaultConfiguration.kt
@@ -0,0 +1,8 @@
+package com.ssu.commerce.delivery.configs
+
+import com.ssu.commerce.vault.config.EnableSsuCommerceVault
+import org.springframework.context.annotation.Configuration
+
+@EnableSsuCommerceVault
+@Configuration
+class VaultConfiguration
